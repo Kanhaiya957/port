c69660d7671a9253217d19df56885286543b9b59
Kanhaiya Kumar Jha <jhak5271@gmail.com>
2025-11-05 15:17:38 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export", // → ensures pure static HTML/CSS/JS
-  images: { unoptimized: true }, // optional, prevents image optimization issues on GitHub Pages
-  basePath: "", // keep empty for root deployment
+  images: { unoptimized: true, }, // optional, prevents image optimization issues on GitHub Pages
+  basePath: '/ https://github.com/Kanhaiya957/port.git
 };
 
-export default nextConfig;
+module.export = nextConfig;
